7693d6b2f561bcacf96271bd1c12fad574075659
ludwig <hello@ludwigvantours.dev>
2023-10-29 00:59:24 +0200

    ReactRootView

--- android/app/src/main/java/com/example/reactnative/brownfield/MainActivity.kt
+++ android/app/src/main/java/com/example/reactnative/brownfield/MainActivity.kt
@@ -61,12 +61,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ReactNativeBrownfieldTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text("hello jetpack compose")
-                }
+
+                    OnboardingCard()
+
             }
         }
     }
