31147776869ef199ac097d0eafa0dde159494b96
ludwig <hello@ludwigvantours.dev>
2023-10-29 10:00:52 +0100

    MyReactActivity

--- android/app/src/main/java/com/example/reactnative/brownfield/MainActivity.kt
+++ android/app/src/main/java/com/example/reactnative/brownfield/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.reactnative.brownfield
 
 import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,20 +20,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -38,10 +36,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.reactnative.brownfield.ui.theme.ReactNativeBrownfieldTheme
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
-import android.provider.Settings
 
 class MainActivity : ComponentActivity() {
     companion object {
--- android/app/src/main/java/com/example/reactnative/brownfield/MyReactActivity.kt
+++ android/app/src/main/java/com/example/reactnative/brownfield/MyReactActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.*
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 import android.app.Activity
 import android.os.Bundle
+import com.example.reactnative.brownfield.BuildConfig.*
 import com.facebook.react.common.LifecycleState
 import com.facebook.soloader.SoLoader
 
@@ -13,27 +14,38 @@ class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
     private lateinit var reactInstanceManager: ReactInstanceManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Dans onCreate, j'initialisez SoLoader. C'est une bibliothèque native qui permet de charger du code natif depuis des fichiers APK.
         SoLoader.init(this, false)
+        // créez une nouvelle instance de ReactRootView qui servira de conteneur pour vos composants React Native.
         reactRootView = ReactRootView(this)
+
         val packages: List<ReactPackage> = PackageList(application).packages
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(MyReactNativePackage())
         // Remember to include them in `settings.gradle` and `app/build.gradle` too.
+
+        // Configuration de ReactInstanceManager, il est configuré avec divers paramètres, comme les assets du bundle JS, le module principal JS, les packages natifs, etc.
         reactInstanceManager = ReactInstanceManager.builder()
             .setApplication(application)
             .setCurrentActivity(this)
             .setBundleAssetName("index.android.bundle")
             .setJSMainModulePath("index")
             .addPackages(packages)
-            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setUseDeveloperSupport(DEBUG)
             .setInitialLifecycleState(LifecycleState.RESUMED)
             .build()
+
+
         // The string here (e.g. "MyReactNativeApp") has to match
         // the string in AppRegistry.registerComponent() in index.js
+        // Ici, vous liez votre ReactRootView avec ReactInstanceManager et indiquez quel composant React Native doit être chargé.
         reactRootView?.startReactApplication(reactInstanceManager, "MyReactNativeApp", null)
+
+        // vous définissez ReactRootView comme vue principale de l'Activity
         setContentView(reactRootView)
     }
 
+    // Cette méthode est appelée lorsque l'utilisateur appuie sur le bouton "Retour".
     override fun invokeDefaultOnBackPressed() {
         super.onBackPressed()
     }
