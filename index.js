d79753b7fe81b4d6105e379825053c3f1add5205
ludwig <hello@ludwigvantours.dev>
2023-10-29 11:24:36 +0100

    add button to quit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, Button, NativeModules, NativeEventEmitter} from 'react-native';
+
+const { MyReactNativeModule } = NativeModules;
+const eventEmitter = new NativeEventEmitter(MyReactNativeModule);
 
 const HelloWorld = () => {
+
+  const handleClose = () => {
+    eventEmitter.emit('closeActivity', {});
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.hello}>Hello, World</Text>
+      <Text style={styles.hello}>Hello, World from react native side</Text>
+      <Button title="Fermer" onPress={handleClose} />
     </View>
   );
 };
@@ -12,6 +21,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    gap: 40,
+    paddingHorizontal: 20,
   },
   hello: {
     fontSize: 20,
